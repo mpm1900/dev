6eb6519d2f1df39d1d30fb2d9310f4001b0ab525
mpm1900 <almostmacmiller@gmail.com>
2015-07-15 14:59:28 -0500

    update

--- app/about/aboutController.js
+++ app/about/aboutController.js
@@ -1,4 +1,4 @@
-angular.module('app').controller('homeController', function($scope) {
+angular.module('app').controller('aboutController', function($scope) {
 	$scope.title = 'About Page';
 	$scope.text = 'This is some text that is for an about page';
 });
