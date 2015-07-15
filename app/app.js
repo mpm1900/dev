52db70270734b25738eca65cf1d8c872df774cc4
mpm1900 <almostmacmiller@gmail.com>
2015-07-15 14:22:55 -0500

    update

--- app/app.js
+++ app/app.js
@@ -1,1 +1,10 @@
-angular.module('app', []);
+angular.module('app', ['ngRoute']);
+
+angular.module('app').config(function($routeProvider) {
+
+	// app url routing
+	$routeProvider.when("/", {
+		templateUrl: base + '/home/home.html',
+		controller: 'HomeController'
+	});
+});
---
+++ app/home/homeController.js
@@ -0,0 +1,4 @@
+angular.module('app').controller(function($scope) {
+	$scope.title = 'Home Page';
+	$scope.text = 'Here is some text loaded from an angular controller!';
+});
