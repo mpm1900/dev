139a59251e53f457228f7d536c881637147add91
mpm1900 <almostmacmiller@gmail.com>
2015-07-15 14:26:10 -0500

    update

--- app/home/homeController.js
+++ app/home/homeController.js
@@ -1,4 +1,4 @@
-angular.module('app').controller(function($scope) {
+angular.module('app').controller('homeController', function($scope) {
 	$scope.title = 'Home Page';
 	$scope.text = 'Here is some text loaded from an angular controller!';
 });
