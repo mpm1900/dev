d7f44f3ff4cdd9ca9033dd472ab9d92b6844f1b8
mpm1900 <almostmacmiller@gmail.com>
2015-07-15 14:39:06 -0500

    update

--- app/app.js
+++ app/app.js
@@ -1,13 +1,11 @@
-angular.module('app', []);
 
-/*
 angular.module('app', ['ngRoute']);
 
-angular.module('app').config(function($routeProvider) {
+angular.module('app').config(['$routeProvider', function($routeProvider) {
 
 	// app url routing
 	$routeProvider.when("/", {
 		templateUrl: base + './home/home.html',
 		controller: 'HomeController'
 	});
-}); */
+}]);
